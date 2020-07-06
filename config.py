501bf40ee8ea411a300589edf82b2ff4a3edb653
MacroBull <MacroBull.Github@Gmail.com>
2020-07-06 16:45:56 +0800

    key escape support; better logging and args support

--- config.py
+++ config.py
@@ -27,7 +27,9 @@ DELIMITER_PATH :str = '.'
 
 PROGRAM_NAME           :str = shutil.os.path.basename(shutil.sys.argv[0])
 DEFAULT_LOGGING_FORMAT :str = (
-        '[%(levelname)8s](%(asctime)8s)%(name)s::%(funcName)s:%(lineno)4d: %(message)s')
+        '[%(levelname)8s](%(asctime)8s)<%(process)5d> '
+        '%(name)s::%(funcName)s @ %(filename)s:%(lineno)d: %(message)s'
+        )
 
 state = dict()
 
@@ -35,7 +37,7 @@ state = dict()
 def is_listy(obj:'Any')->bool:
     r"""is `obj` 'list'-like"""
 
-    return isinstance(obj, (tuple, list, set))
+    return isinstance(obj, (list, tuple, set))
 
 
 def is_dict(obj:'Any')->bool:
@@ -62,31 +64,35 @@ def get_func_name(func:'Callable')->str:
 
 
 def dict2obj(d:'Mapping[str, Any]',
-             cls_name:str='Namespace')->'Any':
+             cls_name:str='Namespace', escapes:str='-')->'Any':
     r"""
-        'dict' to read-only fields recursively
-        if mutable keys wanted, try EasyDict
+    'dict' to read-only fields recursively
+    if mutable keys wanted, try EasyDict
     """
 
-    d = d.copy()
+    d_ = dict()
     for k, v in d.items():
+        for s in escapes:
+            k = k.replace(s, '_')
         if is_dict(v):
-            d[k] = dict2obj(v, cls_name=k)
-    cls = namedtuple(cls_name, d.keys())
-    return cls(**d)
+            d_[k] = dict2obj(v, cls_name=k)
+        else:
+            d_[k] = v
+    cls = namedtuple(cls_name, d_.keys())
+    return cls(**d_)
 
 
 def parse_args(
         args:'Optional[Sequence[str]]'=None,
-        default_config:str='configs/default')->argparse.Namespace:
+        default_config:str='configs/default', description:str=PROGRAM_NAME)->argparse.Namespace:
     r"""the default parse_args"""
 
     parser = argparse.ArgumentParser(
-            description=PROGRAM_NAME,
+            description=description,
             formatter_class=argparse.ArgumentDefaultsHelpFormatter,
             )
     parser.add_argument(
-            '--config', '-c', default=f'{default_config}.yaml',
+            '--config', '-c', default=f'{default_config}.yaml', # required=True,
             help='path to config.yaml',
             )
     parser.add_argument(
@@ -144,7 +150,7 @@ def on_path(func:ConfigTransform, path:str,
     r"""call `func` at `path` in `config`"""
 
     @wraps(func)
-    def _wrap(config, **kwargs):
+    def wrapped(config, *fargs, **fkwargs):
         config = config.copy()
         obj, po, pk = config, None, None
         if path:
@@ -154,15 +160,16 @@ def on_path(func:ConfigTransform, path:str,
                         key = int(key) # throw ValueError
                     if is_dict(obj[key]): # throw KeyError, IndexError
                         obj[key] = obj[key].copy()
-                    po, pk = obj, key
-                    obj = obj[key]
                 except (ValueError, KeyError, IndexError):
-                    logger = state.get('logger', None)
+                    logger = state.get('logger')
                     if logger is not None:
                         logger.warning('bad index %s for %s, transform skipped',
                                        key, obj)
                     return config
-        ret = func(obj, *args, **kwargs)
+                else:
+                    po, pk = obj, key
+                    obj = obj[key]
+        ret = func(obj, *fargs, **fkwargs)
         if po is None:
             config = ret
         else:
@@ -170,23 +177,38 @@ def on_path(func:ConfigTransform, path:str,
         return config
 
     func_name = get_func_name(func)
-    _wrap.__name__ = f'{func_name}(@path={path})'
-    return _wrap
+    wrapped.__name__ = f'{func_name}(@path={path})'
+    return wrapped
 
 
 def setup_global_logging(
         config:ConfigDict,
-        *args, **kwargs)->ConfigDict:
+        *args,
+        logging_format:str=DEFAULT_LOGGING_FORMAT, logging_level:'Optional[int]'=None,
+        **kwargs)->ConfigDict:
     r"""setup global logging level according to "debug" option in `config`"""
 
-    try:
-        import tqdm_color_logging # throw ImportError
-    except ImportError:
-        pass
+    import sys
 
-    logging_format = DEFAULT_LOGGING_FORMAT
-    logging_level = logging.DEBUG if config['debug'] else logging.INFO
-    logging.basicConfig(format=logging_format, level=logging_level)
+    logging_level = logging_level or (logging.DEBUG if config['debug'] else logging.INFO)
+
+    try:
+        from tqdm_color_logging import basicConfig
+    except ImportError:
+        from logging import basicConfig
+
+    kwargs.setdefault('stream', sys.stderr)
+    basicConfig(format=logging_format, level=logging_level, **kwargs)
+
+    if 'filename' not in kwargs:
+        path = config.get('path')
+        log_dir = path and path.get('log_dir')
+        if log_dir and log_dir.is_dir(): 
+            handler = logging.handlers.TimedRotatingFileHandler(
+                (log_dir / PROGRAM_NAME).with_suffix('.log'),
+                when='D', backupCount=5)
+            handler.setFormatter(logging.Formatter(fmt=logging_format, datefmt='%H:%M:%S'))
+            logging.root.addHandler(handler)
 
     state['logger'] = logging.getLogger(__name__)
 
@@ -194,26 +216,29 @@ def setup_global_logging(
 
 
 def mkdirs(config:ConfigDict,
-           *args, **kwargs)->ConfigDict:
+           *args,
+           key_endswith:str='_dir',
+           **kwargs)->ConfigDict:
     r"""create directories for 'Path's in `config`"""
 
-    def _inner(d):
+    def inner(d):
         for k, v in d.items():
             if is_dict(v):
-                _inner(v)
+                inner(v)
                 continue
-            if isinstance(v, (Path, str)) and k.endswith('_dir'):
+            if isinstance(v, (Path, str)) and k.endswith(key_endswith):
                 v = Path(v)
                 if v.is_absolute(): # only resolved
                     shutil.os.makedirs(v, exist_ok=True)
 
-    _inner(config)
+    inner(config)
     return config
 
 
 def convert_rel_path(
         config:ConfigDict,
         *args,
+        rel_key:str='_rel_',
         base_key:str='train_dir',
         **kwargs)->ConfigDict:
     r"""convert 'Path's in `config` if it's relative(with _rel_)"""
@@ -221,58 +246,60 @@ def convert_rel_path(
     base_dir = config[base_key]
     assert isinstance(base_dir, Path), f'base_dir({base_dir}) is not a Path'
 
-    def _inner(d):
+    def inner(d):
         r = type(d)()
         for k, v in d.items():
             if is_dict(v):
-                r[k] = _inner(v)
+                r[k] = inner(v)
                 continue
-            if k.count('_rel_') != 1 or not isinstance(v, (Path, str)):
+            if k.count(rel_key) != 1 or not isinstance(v, (Path, str)):
                 r[k] = v
                 continue
             v_ = Path(v)
             if v_.is_absolute():
                 r[k] = v
                 continue
-            r[k.replace('_rel_', '_')] = base_dir / v
+            r[k.replace(rel_key, '_')] = base_dir / v
         return r
 
-    return _inner(config)
+    return inner(config)
 
 
 def resolve_args(
         args:argparse.Namespace,
         tfms:ConfigTransforms=(setup_global_logging, ),
+        config_role:str='config', default_role:str='default', args_role:str='args',
         config_cls:'Optional[type]'=None,
         )->'Uniont[config, EasyDict]':
     r"""
-        load, resolve and merge from YAML config file
-        `tfms`(transforms): convert_rel_path, mkdirs, setup_global_logging ...
-        `config_cls` can be other types like EasyDict to be used instead of namedtuple as config
+    load, resolve and merge from YAML config file
+    `tfms`(transforms): convert_rel_path, mkdirs, setup_global_logging ...
+    `config_cls` can be other types like EasyDict to be used instead of namedtuple as config
     """
 
-    def _recurse_update(d, s):
+    def recurse_update(d, s):
         for k, v in s.items():
             if is_dict(v):
-                v_ = d.get(k, None)
+                v_ = d.get(k)
                 if is_dict(v_):
-                    _recurse_update(v_, v)
+                    recurse_update(v_, v)
                     continue
             d[k] = v
 
     config = dict()
-    if args.default and shutil.os.path.isfile(args.default):
-        config = yaml.load(open(args.default), Loader=ConfigLoader) or dict()
+    default = getattr(args, default_role, None)
+    if default and shutil.os.path.isfile(default):
+        config = yaml.load(open(default), Loader=ConfigLoader) or dict()
 
-    config_ = yaml.load(open(args.config), Loader=ConfigLoader) or dict()
-    _recurse_update(config, config_)
+    config_ = yaml.load(open(getattr(args, config_role)), Loader=ConfigLoader) or dict()
+    recurse_update(config, config_)
 
     for k, v in args.__dict__.items():
         if k in ('args', ):
             continue
         config[k] = v
 
-    for arg in args.args:
+    for arg in getattr(args, args_role):
         path, _, value = arg.partition(DELIMITER_ARG)
         value = yaml.load(value, Loader=ConfigLoader) or True # HINT: true for switch-on
         keys = path.split('.')
@@ -283,7 +310,7 @@ def resolve_args(
                     obj = obj[int(key)] # throw ValueError, IndexError
                 else:
                     obj_ = obj
-                    obj = obj_.get(key, None) # throw AttributeError
+                    obj = obj_.get(key) # throw AttributeError
                     if obj is None:
                         obj = obj_[key] = dict() # throw TypeError
             except (ValueError, IndexError, AttributeError, TypeError):
@@ -309,9 +336,9 @@ def resolve_args(
 def get_transforms()->ConfigTransforms:
     r"""get the default tfms for config"""
 
-    return (setup_global_logging,
-            on_path(convert_rel_path, 'path'),
+    return (on_path(convert_rel_path, 'path'),
             on_path(mkdirs, 'path'),
+            setup_global_logging,
             )
 
 
@@ -331,5 +358,7 @@ def get_config(
 if __name__ == '__main__':
     from easydict import EasyDict
 
-    config = get_config(config_cls=EasyDict)
-    print(config)
+    config = get_config(
+            tfms=(on_path(mkdirs, 'path'), setup_global_logging),
+            config_cls=EasyDict)
+    logging.getLogger().info('config:\n\t%s', config)
